2ca3c21c90a1f566b7199867a315bd4f34bcb018
all-cloudz <allclouds.lee@gmail.com>
2024-04-17 02:52:23 +0900

    명령어를 통해 환경 변수 값을 구할 수 있도록 스크립트 변경

--- deployments/local/dependencies/run_dependencies_ver1.sh
+++ deployments/local/dependencies/run_dependencies_ver1.sh
@@ -2,8 +2,18 @@
 
 if [[ ! -f ".env" ]];
 then
+  set -a
+  source default.env
+  source mysql.env
+  set +a
+
+  cat default.env | grep -v '^#' | awk -F '=' '{print $1 "=" ENVIRON[$1]}' > .default.env
+  cat mysql.env | grep -v '^#' | awk -F '=' '{print $1 "=" ENVIRON[$1]}' > .mysql.env
+
   touch .env
-  awk -F '=' '{vals[$1]=$2} END {for (var in vals) print var "=" vals[var]}' default.env mysql.env > .env
+  awk -F '=' '{map[$1]=$2} END {for (key in map) print key "=" map[key]}' .default.env .mysql.env | sort >> .env
+
+  rm -f .default.env .mysql.env
 fi
 
 docker-compose convert
